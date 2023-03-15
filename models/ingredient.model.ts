5c8741a9f8e99b020c6a1a6699813b6a225261ff
CristianV350 <cristian.vicol@tarainteractive.com>
2023-03-15 22:06:14 +0200

    ingredient model

--- models/ingredient.model.ts
+++ models/ingredient.model.ts
@@ -49,11 +49,11 @@ Ingredient.init({
   },
   checkpoint_id: {
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: true
   },
   archive_id: {
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: true
   }
 }, {
   sequelize,
